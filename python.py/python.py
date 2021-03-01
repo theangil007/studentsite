bd11c1f6f77b284e9cab4dcd47e1f1f9167eb326
Angil Kshetri <59067083+theangil007@users.noreply.github.com>
2021-03-01 09:46:06 +0545

    Add files via upload

---
+++ python.py/python.py
@@ -0,0 +1,28 @@
+# print ("hello world")
+# print ('hello world')
+# print ('hello "world"')
+# print ("hello 'world'")
+# print ('hello world\"')
+# print ("hello world\'")
+# print ("hello world\\")
+# print ("hello \n world")
+# print ("hello wo\brld")
+# print ("hello wo\trld")
+# print ("\\'" '\\\\"')
+# print ("these are the mountains /\\/\\/\\")
+# print ("i'm a banker")
+# print ("Line A \n LineB"")
+# a = 12
+# b = 13
+# sum = a ** b
+# print (sum)
+# fullname="samir"
+# age="18"
+# print (fullname + " your age is " + age)
+# numb1 = int(input("enter the first number"))
+# numb2 = int(input("enter the second number"))
+# p = numb1 ** numb2
+# print (f"the power of two number is {p}")
+num1,num2,num3 = input ("Enter your number").split(",")
+total = num1+num2+num3
+print (total)
